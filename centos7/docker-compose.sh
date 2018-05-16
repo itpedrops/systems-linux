92519bc90adc6ec7bc808c0978f0332da9309d6e
PedrOps IT <it.pedrops@gmail.com>
2018-05-16 09:10:29 -0300

    Create docker-compose.sh

---
+++ centos7/docker-compose.sh
@@ -0,0 +1,6 @@
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) > ./docker-compose
+
+##
+sudo mv ./docker-compose /usr/bin/docker-compose
+#
+sudo chmod +x /usr/bin/docker-compose
